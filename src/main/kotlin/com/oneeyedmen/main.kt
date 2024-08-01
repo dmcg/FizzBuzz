96373c4f5a4fcbcd84c4e5cbe86a4b1b2737cd38
Duncan McGregor <duncan@oneeyedmen.com>
2024-08-01 15:51:19 +0100

    Working FizzBuzz

--- src/main/kotlin/com/oneeyedmen/main.kt
+++ src/main/kotlin/com/oneeyedmen/main.kt
@@ -1,5 +1,17 @@
 package com.oneeyedmen
 
 fun main() {
+    fizzBuzz(100).forEach { println(it) }
+}
 
+fun fizzBuzz(rounds: Int): List<String> =
+    (1..rounds).map { round ->
+        round.toFizzBuzz()
+    }
+
+internal fun Int.toFizzBuzz(): String = when {
+    this % 15 == 0 -> "FizzBuzz"
+    this % 3 == 0 -> "Fizz"
+    this % 5 == 0 -> "Buzz"
+    else -> toString()
 }
---
+++ src/test/kotlin/com/oneeyedmen/FizzBuzzTests.kt
@@ -0,0 +1,56 @@
+package com.oneeyedmen
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FizzBuzzTests {
+
+    @Test
+    fun `is empty if we have no rounds`() {
+        assertEquals(emptyList(), fizzBuzz(0))
+    }
+
+    @Test
+    fun `is just numbers up to 2`() {
+        assertEquals(
+            listOf("1", "2"),
+            fizzBuzz(2)
+        )
+    }
+
+    @Test
+    fun `3 is Fizz`() {
+        assertEquals(
+            listOf("1", "2", "Fizz"),
+            fizzBuzz(3)
+        )
+    }
+
+    @Test
+    fun `5 is Buzz`() {
+        assertEquals(
+            listOf("1", "2", "Fizz", "4", "Buzz"),
+            fizzBuzz(5)
+        )
+    }
+
+    @Test
+    fun `divisible by 3 is Fizz`() {
+        assertEquals("Fizz", 3.toFizzBuzz())
+        assertEquals("Fizz", 6.toFizzBuzz())
+        assertEquals("Fizz", 9.toFizzBuzz())
+    }
+
+    @Test
+    fun `divisible by 5 is Buzz`() {
+        assertEquals("Buzz", 5.toFizzBuzz())
+        assertEquals("Buzz", 10.toFizzBuzz())
+    }
+
+    @Test
+    fun `divisible by 15 is FizzBuzz`() {
+        assertEquals("FizzBuzz", 15.toFizzBuzz())
+        assertEquals("FizzBuzz", 30.toFizzBuzz())
+    }
+}
+
